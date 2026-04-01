fc8de9e6acb3112ceb052bf9aaaf0e2ceee60c59
mraees93 <raeesdolley76@gmail.com>
2026-04-01 12:15:42 +0200

    abstraction practice

--- OOP_Basics/Program.cs
+++ OOP_Basics/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using OOP_Basics.abstraction;
+ Vehicle myCar = new Car();
+        myCar.Start();
+        myCar.Stop();
+
+        Vehicle myBike = new Bike();
+        myBike.Start();
+        myBike.Stop();
---
+++ OOP_Basics/abstraction/Vehicle.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// Abstraction hides unecessary details and only shows important details to the user
+namespace OOP_Basics.abstraction
+{
+    abstract class Vehicle
+    {
+        //abstract method (must be implemented by derived classes)
+        public abstract void Start();
+
+        //concrete method (already implemented)
+        public void Stop()
+        {
+            Console.WriteLine("Vehicle stopped.");
+        }
+    }
+
+//car class inherits from vehicle
+class Car : Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("Car is starting.");
+    }
+}
+
+//bike class inherits from vehicle
+class Bike : Vehicle
+{
+    public override void Start()
+    {
+        Console.WriteLine("Bike is starting.");
+    }
+}
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Vehicle myCar = new Car();
+//         myCar.Start();
+//         myCar.Stop();
+
+//         Vehicle myBike = new Bike();
+//         myBike.Start();
+//         myBike.Stop();
+//     }
+// }
+}
