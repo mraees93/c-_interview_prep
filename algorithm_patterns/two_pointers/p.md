f311c2f5f034c023c3246dd16291cbd55601ebf2
mraees93 <raeesdolley76@gmail.com>
2026-05-21 16:19:42 +0200

    sql practice

---
+++ algorithm_patterns/two_pointers/p.md
@@ -0,0 +1,16 @@
+get triplets where it equals zero
+
+Input: nums = [-1, 0, 1, 2, -1, -4]
+Expected Output: [[-1, -1, 2], [-1, 0, 1]]
+
+
+Input: nums = [0, 0, 0, 0]
+Expected Output: [[0, 0, 0]]
+
+
+res=[ ]
+i=2
+l=3
+r=5
+
+threesum=-5
---
+++ sql/basics_practice.sql
@@ -0,0 +1,22 @@
+-- Schema Details:
+-- Lawyers (LawyerID, Name, Department)
+-- Matters (MatterID, Title, LeadLawyerID)
+-- Documents (DocID, MatterID, FileSizeKB)
+
+-- 1. List all Lawyers and their total file storage. If a lawyer has no files, show 0 instead of NULL
+
+SELECT l.Name, COALESCE(SUM(d.FileSizeKB), 0) AS TotalFileStorage
+FROM Lawyers l
+LEFT JOIN Matters m ON l.LawyerID = m.LeadLawyerID
+LEFT JOIN Documents d ON m.MatterID = d.MatterID
+GROUP BY l.LawyerID, l.Name;
+
+-- 2. Find the Names of Lawyers who have at least one Matter, but that Matter has zero documents.
+
+SELECT DISTINCT l.Name
+FROM Lawyers l
+JOIN Matters m ON l.LawyerID = m.LeadLawyerID
+LEFT JOIN Documents d ON m.MatterID = d.MatterID
+WHERE d.DocID IS NULL;
+
+-- 3. Calculate total documents per Lawyer, ensuring that if two lawyers have the same name, they are not combined.
