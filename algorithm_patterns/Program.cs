f9d0e29c7d2c6f433561b4b0176b002a98285a1b
mraees93 <raeesdolley76@gmail.com>
2026-06-10 13:13:03 +0200

    algorithms practice

---
+++ algorithm_patterns/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+// This lets Program.cs find your HashMapBucket class
+using algorithm_patterns.Hash_Map_Frequency; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. Create an instance of your class
+        HashMapBucket solver = new HashMapBucket();
+
+        // 2. Set up the example input array
+        string[] input = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+        // 3. Call your method and store the result
+        IList<IList<string>> result = solver.GroupAnagrams(input);
+
+        // 4. Print the result to the screen so you can see it
+        Console.WriteLine("--- Anagram Groups ---");
+        foreach (var group in result)
+        {
+            Console.WriteLine("[" + string.Join(", ", group) + "]");
+        }
+    }
+}
