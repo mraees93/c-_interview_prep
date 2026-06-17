d605317082c917b1a928548f2e688c1b9ed4a73d
mraees93 <raeesdolley76@gmail.com>
2026-06-17 11:50:22 +0200

    basics practice

---
+++ c#_basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace c__basics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static string WhatsYourName()
+    {
+        var sb = new StringBuilder();
+        
+    }
+}
