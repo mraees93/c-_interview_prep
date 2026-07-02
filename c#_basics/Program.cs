5aa8b8244336766dbb66ed43a7d19ae976ffc17e
mraees93 <raeesdolley76@gmail.com>
2026-07-02 11:32:33 +0200

    basics practice

--- c#_basics/Program.cs
+++ c#_basics/Program.cs
@@ -62,5 +62,29 @@ public class Program
         // del += SayBye; //array queue holding [SayHi, SayBye]
         // del -= SayHi; //unlinks and leaves only [SayBye]
         // del();
+
+        //Implicit Casting (automatically) - converting a smaller type to a larger type size: char -> int -> long -> float -> double
+        // int myInt = 9;
+        // double myDouble = myInt;       // Automatic casting: int to double
+
+        // Console.WriteLine(myInt);    
+        // Console.WriteLine(myDouble); 
+
+        //Explicit Casting (manually) - converting a larger type to a smaller size type: double -> float -> long -> int -> char
+        double myDouble = 9.78;
+        int myInt = (int) myDouble;    // Manual casting: double to int
+
+        Console.WriteLine(myDouble);   // Outputs 9.78
+        Console.WriteLine(myInt);      // Outputs 9
+
+        //Type conversion methods
+        // int myInt = 10;
+        // double myDouble = 5.25;
+        // bool myBool = true;
+
+        // Console.WriteLine(Convert.ToString(myInt));    
+        // Console.WriteLine(Convert.ToDouble(myInt));    
+        // Console.WriteLine(Convert.ToInt32(myDouble)); 
+        // Console.WriteLine(Convert.ToString(myBool));
     }
 }
