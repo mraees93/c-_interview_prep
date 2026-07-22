2b42d73cedf41b83c279f71ed360b3b08abe02db
mraees93 <raeesdolley76@gmail.com>
2026-07-22 14:42:22 +0200

    basics practice

--- c_sharp_basics_practice/Program.cs
+++ c_sharp_basics_practice/Program.cs
@@ -1,3 +1,4 @@
 ﻿using c_sharp_basics_practice.practice;
 
-
+ConsoleLogger logger = new ConsoleLogger();
+logger.Log("Hello Raees");
--- c_sharp_basics_practice/practice/practice.cs
+++ c_sharp_basics_practice/practice/practice.cs
@@ -1,2 +1,14 @@
 namespace c_sharp_basics_practice.practice;
 
+public interface ILogger
+{
+    void Log(string message, string level = "Info");
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message, string level = "Info")
+    {
+        System.Console.WriteLine($"[{level}] {message}");
+    }
+}
