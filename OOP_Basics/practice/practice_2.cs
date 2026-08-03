1e61b7c819875c89593582288b2e269b16a81fe3
mraees93 <raeesdolley76@gmail.com>
2026-08-03 11:50:47 +0200

    oop practice

--- OOP_Basics/practice/practice_2.cs
+++ OOP_Basics/practice/practice_2.cs
@@ -3,11 +3,11 @@ namespace OOP_Basics.practice;
 public class LegalEngine
 {
     public virtual void Process() => Console.WriteLine("Base Engine");
-    public void Run() => Console.WriteLine("Base Run");
+    public virtual void Run() => Console.WriteLine("Base Run");
 }
 
 public class SpecializedSearchEngine : LegalEngine
 {
     public override void Process() => Console.WriteLine("Specialized Engine");
-    public new void Run() => Console.WriteLine("Specialized Run");
+    public override void Run() => Console.WriteLine("Specialized Run");
 }
