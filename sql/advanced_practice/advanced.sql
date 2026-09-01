d3523e22aa59ee2746e75fdac66ab76722c58989
mraees93 <raeesdolley76@gmail.com>
2026-09-01 13:38:02 +0200

    sql practice

--- sql/advanced_practice/advanced.sql
+++ sql/advanced_practice/advanced.sql
@@ -1,11 +1,23 @@
---9. Write a query to show the CandidateID, CheckType, and CostZAR, alongside a new column displaying the average cost of that specific CheckType across the entire 
--- database.
-
 -- Schema Details:
 -- Clients (ClientID, CompanyName, Industry)
 -- Candidates (CandidateID, ClientID, FullName, SubmissionDate)
 -- Verifications (CheckID, CandidateID, CheckType, CostZAR, Status)
 -- VerificationLogs (LogID, CheckID, ActionTaken, LogTimestamp)
 
+--9. Write a query to show the CandidateID, CheckType, and CostZAR, alongside a new column displaying the average cost of that specific CheckType across the entire 
+-- database.
+
+SELECT CandidateID, CheckType, CostZAR,
+      AVG(CostZAR) OVER(PARTITION BY CheckType) AS AvgCostOfCheckType
+FROM Verifications;
+
 --10. Select the CandidateID, FullName, and SubmissionDate, along with a column that assigns a sequential row number to candidates for each unique client, 
 -- ordered by their SubmissionDate from earliest to latest.
+
+SELECT CandidateID, FullName, SubmissionDate,
+      ROW_NUMBER() OVER(PARTITION BY ClientID ORDER BY SubmissionDate ASC) AS CandidatesBySubmissionDate
+FROM Candidates;
+
+SELECT CandidateID, FullName, SubmissionDate,
+    ROW_NUMBER() OVER(PARTITION BY ClientID ORDER BY SubmissionDate ASC) AS CandidatesBySubmissionDate
+FROM Candidates;
