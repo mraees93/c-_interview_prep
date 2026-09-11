0d726e1a8d59447e496535fda6da31dbb222b871
mraees93 <raeesdolley76@gmail.com>
2026-09-11 12:12:08 +0200

    angular practice

--- frontend/angular/src/components/api-consumption/api-consumption.component.ts
+++ frontend/angular/src/components/api-consumption/api-consumption.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { Component, computed, effect, ElementRef, inject, OnDestroy, OnInit, output, Signal, signal, viewChild, WritableSignal } from '@angular/core';
 import { CommentLog } from '../../models/comment-log.model';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
     selector: 'app-api-consumption',
@@ -18,22 +19,42 @@ export class ApiConsumptionComponent {
     searchTerm = signal<string>('');
     sortDirection = signal<'ASC' | 'DESC'>('ASC');
 
-    // base consumer - grabs all 500 records once
-    // rxResource (like useQuery() => TanStack Query)
-    rawCommentsResource = rxResource({
-        stream: () => this.http.get<CommentLog[]>('https://jsonplaceholder.typicode.com/comments')
-    });
+    comments = signal<CommentLog[]>([]);
+    isLoading = signal<boolean>(false);
+    error = signal<string | null>(null);
+
+    constructor() {
+        //Native effect() (like useEffect(() => {}, []))
+        // fires once when component is painted onto the DOM
+
+        effect(async () => {
+            try {
+                this.isLoading.set(true);
+                this.error.set(null);
+
+                const data = await firstValueFrom(
+                    this.http.get<CommentLog[]>('https://jsonplaceholder.typicode.com/comments')
+                );
+
+                this.comments.set(data);
+            } catch (err: any) {
+                this.error.set(err.message || 'Failed to populate from API');
+            } finally {
+                this.isLoading.set(false);
+            }
+        })
+    }
 
     //high performance sort & filter pipeline (like useMemo)
     processedComments = computed(() => {
         //fallbacks if data is still streaming down from network
-        const list = this.rawCommentsResource.value() || [];
+        const list = this.comments();
         const search = this.searchTerm().toLowerCase().trim();
         const isAscending = this.sortDirection() === 'ASC';
 
         let result = list;
         if(search) {
-            result = list.filter(item => {
+            result = result.filter(item => {
                 const nameMatch = item.name ? item.name.toLowerCase().includes(search) : false;
                 const emailMatch = item.email ? item.email.toLowerCase().includes(search) : false;
                 return nameMatch || emailMatch;
